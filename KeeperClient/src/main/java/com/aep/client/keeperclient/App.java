b83b5c03167b55b9c81b67a31493c965e399bc34
bao.li.net <bao.li.net@gmail.com>
2012-12-14 15:37:35 +0800

    All projects created

---
+++ KeeperClient/src/main/java/com/aep/client/keeperclient/App.java
@@ -0,0 +1,13 @@
+package com.aep.client.keeperclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
